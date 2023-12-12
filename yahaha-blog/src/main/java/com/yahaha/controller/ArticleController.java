12c5c5d9f58221a467bced89a73119875af0d57a
ybq <binquan.ybq@alibaba-inc.com>
2023-12-12 18:30:11 +0800

    feat: update category

--- yahaha-blog/src/main/java/com/yahaha/controller/ArticleController.java
+++ yahaha-blog/src/main/java/com/yahaha/controller/ArticleController.java
@@ -1,15 +1,12 @@
 package com.yahaha.controller;
 
 import com.yahaha.domain.ResponseResult;
-import com.yahaha.domain.entity.Article;
 import com.yahaha.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Field;
-import java.util.List;
 
 @RestController
 @RequestMapping("/article")
@@ -18,7 +15,7 @@ public class ArticleController {
     private ArticleService articleService;
 
     @GetMapping("/hotArticleList")
-    public ResponseResult<Article> hotArticleList() {
+    public ResponseResult hotArticleList() {
         // 查询热门文章, 封装成 ResponseResult 返回
         return articleService.hotArticleList();
     }
